f03ce2a91f648a3108c731d1c354dd214031767a
Leonardo Almeida <leonardoa.almeida@hotmail.com>
2021-07-22 00:11:19 -0300

    Criando arquivo de rotas e instanciando no express

---
+++ app/routes/foto.js
@@ -0,0 +1,18 @@
+module.exports = function (app) {
+    app.get("/v1/fotos", function (req, res) {
+        var fotos = [
+            {
+                _id: 1,
+                titulo: "Leão",
+                url: "http://www.fundosanimais.com/Minis/leoes.jpg",
+            },
+            {
+                _id: 2,
+                titulo: "Leão 2",
+                url: "http://www.fundosanimais.com/Minis/leoes.jpg",
+            },
+        ];
+
+        res.json(fotos);
+    });
+};
--- config/express.js
+++ config/express.js
@@ -3,31 +3,7 @@ var app = express();
 
 app.use(express.static("./public"));
 
-app.get("/v1/fotos", function (req, res) {
-    var fotos = [
-        {
-            _id: 1,
-            titulo: "Leão",
-            url: "http://www.fundosanimais.com/Minis/leoes.jpg",
-        },
-        {
-            _id: 2,
-            titulo: "Leão 2",
-            url: "http://www.fundosanimais.com/Minis/leoes.jpg",
-        },
-    ];
-
-    res.json(fotos);
-});
-
-app.get("/v1/grupos", function (req, res) {
-    var grupos = [
-        { _id: 1, nome: "esportes" },
-        { _id: 2, nome: "lugares" },
-        { _id: 3, nome: "animais" },
-    ];
-
-    res.json(grupos);
-});
+require("../app/routes/foto")(app);
+require("../app/routes/grupo")(app);
 
 module.exports = app;
