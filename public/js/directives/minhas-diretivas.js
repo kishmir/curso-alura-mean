2a0fd08419d5c1305771e3046d043caa75197269
Leonardo Almeida <leonardoa.almeida@hotmail.com>
2021-07-21 23:59:31 -0300

    Indentaçao do prettify

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,43 +1,43 @@
-angular.module('minhasDiretivas', [])
-	.directive('meuPainel', function() {
+angular
+    .module("minhasDiretivas", [])
+    .directive("meuPainel", function () {
+        var ddo = {};
 
-		var ddo = {};
-
-		ddo.restrict = "AE";
+        ddo.restrict = "AE";
         ddo.transclude = true;
 
-
-		ddo.scope = {
-            titulo: '@'
+        ddo.scope = {
+            titulo: "@",
         };
 
-        ddo.templateUrl = 'js/directives/meu-painel.html';
+        ddo.templateUrl = "js/directives/meu-painel.html";
 
-		return ddo;
-	})
-    .directive('minhaFoto', function() {
-
+        return ddo;
+    })
+    .directive("minhaFoto", function () {
         var ddo = {};
 
         ddo.restrict = "AE";
 
         ddo.scope = {
-            titulo: '@',
-            url: '@'
+            titulo: "@",
+            url: "@",
         };
 
-        ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
-        
+        ddo.template =
+            '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';
+
         return ddo;
     })
-    .directive('meuBotaoPerigo', function() {
+    .directive("meuBotaoPerigo", function () {
         var ddo = {};
         ddo.restrict = "E";
         ddo.scope = {
-            nome: '@',
-            acao : '&'
-        }
-        ddo.template = '<button class="btn btn-danger btn-block" ng-click="acao()">{{nome}}</button>';
+            nome: "@",
+            acao: "&",
+        };
+        ddo.template =
+            '<button class="btn btn-danger btn-block" ng-click="acao()">{{nome}}</button>';
 
         return ddo;
     });
