dbf7fc0a03605191b8a42fa6dc3dc26eba70b2dd
Leonardo Almeida <leonardoa.almeida@hotmail.com>
2021-07-29 01:27:54 -0300

    Preparando conexao ao BD

--- config/database.js
+++ config/database.js
@@ -1,7 +1,24 @@
-var mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/alurapic");
-
-mongoose.connection.on("connected", function() {
-    console.log("Conectado ao BD");
-});
+module.exports = function(uri) {
+    var mongoose = require("mongoose");
+    
+    mongoose.connect("mongodb://" + uri);
+    
+    mongoose.connection.on("connected", function () {
+        console.log("Conectado ao BD");
+    });
+    
+    mongoose.connection.on("error", function (error) {
+        console.log("Erro na conexão do BD: " + error);
+    });
+    
+    mongoose.connection.on("disconnected", function () {
+        console.log("Desconectado do BD");
+    });
+    
+    process.on("SIGINT", function () {
+        mongoose.connection.close(function () {
+            console.log("Conexão fechada pelo término da aplicação");
+            process.exit(0);
+        });
+    });
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var app = require("./config/express");
-require("./config/database");
+require("./config/database")("localhost/alurapic");
 
 http.createServer(app).listen(3000, function () {
     console.log("Iniciado");
